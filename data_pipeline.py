e66b09bdf081a0605b44debceb55facc2fadd237
nilyeh <nilyeh@hotmail.com>
2024-09-11 20:48:00 -0600

    data_pipeline.py se corrige 21, 18 y 35

--- data_pipeline.py
+++ data_pipeline.py
@@ -18,18 +18,18 @@ t0: time.time()
 getVideoTranscripts()
 t1 = time.time()
 print ("Step 2: Done")
-print ("----> Transcripts downloaded in", str(t1-t0), "seconds", "\n" ")
+print ("----> Transcripts downloaded in", str(t1-t0), "seconds", "\n" )
 
 #Step 3: Transform data
 t0: time.time()
 transformData()
 t1 = time.time()
 print ("Step 3: Done")
-print ("----> Data transformed downloaded in", str(t1-t0), "seconds", "\n" ")
+print ("----> Data transformed downloaded in", str(t1-t0), "seconds", "\n" )
 
 #Step 4: Generate text embeddings
 t0: time.time()
 createTexEmbeddings()
 t1 = time.time()
 print ("Step 4: Done")
-print ("----> Embeddings generated in", str(t1-t0), "seconds", "\n" ")
+print ("----> Embeddings generated in", str(t1-t0), "seconds", "\n" )
